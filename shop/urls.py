42484b9b5d0323ad8dedec90e30bdceab3b9d1bc
akiyoko <akiyoko@users.noreply.github.com>
2021-07-16 07:22:51 +0900

    PaymentIntents API 対応

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,6 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:book_id>/', views.detail, name='detail'),
-    path('complete/', TemplateView.as_view(template_name='shop/complete.html'), name='complete'),
+    path('create-payment-intent/', views.payment, name='payment'),
+    # path('complete/', TemplateView.as_view(template_name='shop/complete.html'), name='complete'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,32 +1,32 @@
+import json
 import logging
-import time
+# import time
 
 import stripe
 from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
-from django.urls import reverse
+# from django.urls import reverse
 from django.views.generic import View
 
 from .models import Book
 
+stripe.api_key = settings.STRIPE_API_KEY
 logger = logging.getLogger(__name__)
 
 
 class IndexView(View):
     # class IndexView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        start_time = time.time()
-        logger.info(f"User({request.user.id}) posted.")
+        # start_time = time.time()
+        # logger.info(f"User({request.user.id}) posted.")
         queryset = Book.objects.select_related('publisher').prefetch_related(
             'authors').order_by('publish_date')
         keyword = request.GET.get('keyword')
         if keyword:
-            # TODO
-            messages.info(request, "TODO")
             queryset = queryset.filter(
                 Q(title__icontains=keyword) | Q(description__icontains=keyword)
             )
@@ -34,7 +34,7 @@ class IndexView(View):
             'keyword': keyword,
             'book_list': queryset,
         }
-        logger.debug(f"Finished in {time.time() - start_time:.2f} sec.")
+        # logger.debug(f"Finished in {time.time() - start_time:.2f} sec.")
         return TemplateResponse(request, 'shop/book_list.html', context)
 
 
@@ -45,25 +45,25 @@ class DetailView(LoginRequiredMixin, View):
     def get(self, request, book_id, *args, **kwargs):
         book = get_object_or_404(Book, pk=book_id)
 
-        stripe.api_key = settings.STRIPE_API_KEY
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price_data': {
-                    'product': 'prod_JrHxyqQP0vTHeR',
-                    'unit_amount': book.price,
-                    'currency': 'jpy',
-                },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=request.build_absolute_uri(reverse('shop:complete')),
-            cancel_url=request.build_absolute_uri(reverse('shop:detail', args=(book_id,))),
-        )
+        # Charges API（旧API）を利用した場合
+        # session = stripe.checkout.Session.create(
+        #     payment_method_types=['card'],
+        #     line_items=[{
+        #         'price_data': {
+        #             'product': 'prod_JrHxyqQP0vTHeR',
+        #             'unit_amount': book.price,
+        #             'currency': 'jpy',
+        #         },
+        #         'quantity': 1,
+        #     }],
+        #     mode='payment',
+        #     success_url=request.build_absolute_uri(reverse('shop:complete')),
+        #     cancel_url=request.build_absolute_uri(reverse('shop:detail', args=(book_id,))),
+        # )
         context = {
             'book': book,
-            'checkout_session_id': session.id,
             'stripe_pub_key': settings.STRIPE_PUB_KEY,
+            # 'checkout_session_id': session.id,
         }
         return TemplateResponse(request, 'shop/book_detail.html', context)
 
@@ -71,9 +71,26 @@ class DetailView(LoginRequiredMixin, View):
 detail = DetailView.as_view()
 
 
-class CompleteView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, 'shop/book_detail.html')
+class CreatePaymentAjaxView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        body = json.loads(request.body.decode())
+        book_id = body.get('item', {}).get('id')
+        book = get_object_or_404(Book, pk=book_id)
+
+        intent = stripe.PaymentIntent.create(
+            amount=book.price,
+            currency='jpy'
+        )
+        return JsonResponse({
+            'clientSecret': intent['client_secret']
+        })
 
 
-complete = CompleteView.as_view()
+payment = CreatePaymentAjaxView.as_view()
+
+# class CompleteView(LoginRequiredMixin, View):
+#     def get(self, request, *args, **kwargs):
+#         return TemplateResponse(request, 'shop/book_detail.html')
+#
+#
+# complete = CompleteView.as_view()
